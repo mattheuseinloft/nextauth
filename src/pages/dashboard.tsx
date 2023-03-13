a430db0020c9f36b17aa7b65950acb6a6aa76fff
Mattheus Einloft <mattheuseinloft@gmail.com>
2023-03-13 20:14:50 -0300

    feat: retrieve authentication state

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,13 @@
-import { useContext } from "react";
+import { api } from "../services/api";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response));
+  }, []);
 
   return (
     <h1>Dashboard: {user?.email}</h1>
